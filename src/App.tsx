0979f99c8ca89cd52ee08e320198b0549cfb0279
ALsJourney <aldin.dino@outlook.com>
2023-02-12 21:36:21 +0100

    Finished the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import IpForm from './components/IpForm'
+import ResultDiv from "./components/ResultDiv";
 
 function App() {
 
   return (
     <div className="App">
       <IpForm />
+      <ResultDiv/>
     </div>
   )
 }
---
+++ src/components/ResultDiv.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const ResultDiv: React.FC = () => {
+
+    const ipget: string = "https://geo.ipify.org/api/v2/country?apiKey=at_9jIiqrQhvHH3UjEjLG32Xm3ghLUp2&ipAddress=8.8.8.8"; 
+
+    return (
+        <div>
+            <h1>Sanity check</h1>
+        </div>
+    )
+}
+
+export default ResultDiv;
